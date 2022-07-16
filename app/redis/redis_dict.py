52f20c82121b53fc24573535dc6704456d92d91c
bruno5barros <67475745+bruno5barros@users.noreply.github.com>
2022-07-16 18:25:13 +0200

    find content keys based on value

--- app/redis/redis_dict.py
+++ app/redis/redis_dict.py
@@ -13,3 +13,6 @@ class RedisDict:
     def unset_content(self, key):
         key = tuple(key) if isinstance(key, (list, set, dict)) else key
         return self._contents.pop(key, None)
+
+    def find_content(self, value):
+        return [key for key, dict_value in self._contents.items() if dict_value == value]
