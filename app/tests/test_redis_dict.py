cde4e20b2b082394ec2dd8492372db58184ec284
bruno5barros <67475745+bruno5barros@users.noreply.github.com>
2022-07-16 11:33:55 +0200

    Update the test for all mutable data structure and less code.

--- app/tests/test_redis_dict.py
+++ app/tests/test_redis_dict.py
@@ -8,37 +8,32 @@ class TestRedisDict:
         redis_dict = RedisDict()
         redis_dict._contents = {1: "First redis element"}
 
-        content_value = redis_dict.get_content(1)
-
-        assert redis_dict._contents.get(1, None) == content_value
+        assert redis_dict._contents.get(1, None) == redis_dict.get_content(1)
 
     def test_get_content_does_not_exit(self):
         redis_dict = RedisDict()
-        content_value = redis_dict.get_content(key=1)
 
-        assert None == content_value
+        assert None == redis_dict.get_content(1)
 
     def test_add_content_successfuly(self):
         redis_dict = RedisDict()
         redis_dict.set_content(1, "First redis element")
 
-        content_value = redis_dict._contents.get(1)
-
-        assert redis_dict._contents.get(1, None) == content_value
+        assert redis_dict._contents.get(1, None) == redis_dict.get_content(1)
 
     def test_add_content_with_existing_key(self):
         redis_dict = RedisDict()
         redis_dict.set_content(1, "First redis element")
-        redis_dict.set_content(1, "Second redis element")
+        redis_dict.set_content(1, "Updated redis element")
 
-        content_value = redis_dict._contents.get(1)
-
-        assert "Second redis element" == content_value
+        assert "Updated redis element" == redis_dict.get_content(1)
 
     def test_add_content_mutable_key(self):
         redis_dict = RedisDict()
         redis_dict.set_content([1], "First redis element")
+        redis_dict.set_content({2}, "Second redis element")
+        redis_dict.set_content({3: 3}, "Third redis element")
 
-        content_value = redis_dict._contents.get((1,))
-
-        assert "First redis element" == content_value
+        assert "First redis element" == redis_dict._contents.get((1,))
+        assert "Second redis element" == redis_dict._contents.get((2,))
+        assert "Third redis element" == redis_dict._contents.get((3,))
