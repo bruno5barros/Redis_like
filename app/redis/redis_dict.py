63cb0cb3bd732124183525da4f2664ed8c088b7a
bruno5barros <67475745+bruno5barros@users.noreply.github.com>
2022-07-16 11:25:35 +0200

    Add functionality to add contents to the dict

--- app/redis/redis_dict.py
+++ app/redis/redis_dict.py
@@ -5,3 +5,7 @@ class RedisDict:
 
     def get_content(self, key):
         return self._contents.get(key)
+
+    def set_content(self, key, value):
+        key = tuple(key) if isinstance(key, (list, set, dict)) else key
+        self._contents[key] = value
