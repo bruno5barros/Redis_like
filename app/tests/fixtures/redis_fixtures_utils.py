914d02eab4b53548200cdea3a2d05aa69abe5d04
bruno5barros <67475745+bruno5barros@users.noreply.github.com>
2022-07-18 22:09:53 +0200

    Improve the fixtures with inheritance

--- app/tests/fixtures/redis_fixtures_utils.py
+++ app/tests/fixtures/redis_fixtures_utils.py
@@ -4,14 +4,6 @@ from tests.constants import REDIS_FIRST_ELEMENT
 
 
 @pytest.fixture
-def redis_dict_one_item():
-    redis_dict = RedisDict()
-    redis_dict._contents = {1: REDIS_FIRST_ELEMENT}
-
-    return redis_dict
-
-
-@pytest.fixture
 def redis_dict():
     redis_dict = RedisDict()
 
@@ -19,10 +11,15 @@ def redis_dict():
 
 
 @pytest.fixture
-def redis_dict_begin_trans():
-    redis_dict = RedisDict()
+def redis_dict_one_item(redis_dict):
     redis_dict._contents = {1: REDIS_FIRST_ELEMENT}
-    redis_dict._redis_dict_state = True
-    redis_dict.begin_transactions()
 
     return redis_dict
+
+
+@pytest.fixture
+def redis_dict_begin_trans(redis_dict_one_item):
+    redis_dict_one_item._redis_dict_state = True
+    redis_dict_one_item.begin_transactions()
+
+    return redis_dict_one_item
